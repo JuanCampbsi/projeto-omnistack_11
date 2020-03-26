aee782b446527b925fa6d45e0995c3abc3f25ee1
Juan <juan12_jovem@outlook.com>
2020-03-26 12:01:00 -0300

    Criando Routes e Controllers da aplicação.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,35 +1,21 @@
 const express = require('express');
-const crypto = require ('crypto');
-const connection = require('./database/connection');
 
-
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.get('/ongs', async(request, response) =>{
-    const ongs = await connection('ongs').select('*');
+routes.post('/sessions', SessionController.create);
 
-    return response.json(ongs);
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
+routes.get('/profile', ProfileController.index);
 
-routes.post('/ongs', async (request, response) => {
-    const {name, email, whatsapp, city, uf} = request.body;
+routes.get('/incidents', IncidentController.index); 
+routes.post('/incidents', IncidentController.create); 
+routes.delete('/incidents/:id', IncidentController.delete);
 
-    const id = crypto.randomBytes(4).toString('HEX');
-  
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-
-    })
-
-      return response.json({ id });
-
-  });
-
-  module.exports = routes;
+module.exports = routes;
